a207605d4983ec79c917828def51ba25d70fe9c6
franc <franc@DESKTOP-FDHROCO>
2026-03-07 22:07:06 -0600

    Semana 6: asociaciones

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -5,31 +5,38 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="categoria")
+@Table(name = "categoria")
 public class Categoria implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
+    @Column(name = "id_categoria")
     private Integer idCategoria;
 
-    @Column(unique=true, nullable=false, length=50)
+    @Column(unique = true, nullable = false, length = 50)
     @NotNull
-    @Size(max=50)
+    @Size(max = 50)
     private String descripcion;
 
-    @Column(length=1024)
-    @Size(max=1024)
+    @Column(length = 1024)
+    @Size(max = 1024)
     private String rutaImagen;
 
     private boolean activo;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+    
 }
--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
@@ -17,24 +19,25 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="producto")
+@Table(name = "producto")
 public class Producto implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_producto")
+    @Column(name = "id_producto")
     private Integer idProducto;
-    private Integer idCategoria;
-    
-    @Column(nullable=false, length=50)
-    @NotBlank(message= "La descripción no puede estar vacía")
-    @Size(max=50, message= "La descripción no puede tener más de 50 caracteres")
+    // private Integer idCategoria;ya no se ocupa
+
+    @Column(nullable = false, length = 50)
+    @NotBlank(message = "La descripción no puede estar vacía")
+    @Size(max = 50, message = "La descripción no puede tener más de 50 caracteres")
     private String descripcion;
-    
-    @Column(columnDefinition="TEXT")
+
+    @Column(columnDefinition = "TEXT")
     private String detalle;
-    
+
     @Column(precision = 12, scale = 2)
     @NotNull(message = "El precio no puede estar vacío")
     @DecimalMin(value = "0.01", inclusive = true, message = "El precio debe ser...")
@@ -44,9 +47,14 @@ public class Producto implements Serializable {
     @Min(value = 0, message = "Las existencias deben ser mayor o igual a 0")
     private Integer existencias;
 
-    @Column(length=1024)
-    @Size(max=1024)
+    @Column(length = 1024)
+    @Size(max = 1024)
     private String rutaImagen;
 
     private boolean activo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
+
 }
